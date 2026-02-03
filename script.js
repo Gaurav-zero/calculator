c1b3dec8214139cf13950940d1050cd735fa5e37
Gaurav <183895839+Gaurav-zero@users.noreply.github.com>
2026-02-03 16:38:16 +0530

    add functionality to operator buttons
    added functions and event listeners to operators

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let a="0";
-let b="0";
-let operator;
+let a=null;
+let b=null;
+let operator=null;
 
-let digits= document.querySelectorAll(".digit")
+let btns= document.querySelectorAll(".digit")
 let displayText= document.querySelector(".display h1");
+let chosenOperator= document.querySelectorAll(".operators");
 
 
-function add(a,b){return a+b};
-function subtract(a,b){return a-b};
-function multiply(a,b){return a*b};
-function divide(a,b){return a/b};
+function add(a,b){return Number(a)+Number(b)};
+function subtract(a,b){return Number(a)-Number(b)};
+function multiply(a,b){return Number(a)*Number(b)};
+function divide(a,b){return Number(a)/Number(b)};
 function operate(operator, a, b){
     switch (operator){
         case "+":
@@ -27,18 +28,36 @@ function operate(operator, a, b){
     }
 }
 function storeDigit(value){
-    b= b+value;
+    if(b== null) b=value;
+    else b= b+value;
 }
 
 function updateDisplay(value){
     displayText.textContent= value;
 }
 
-let btns= document.querySelectorAll(".digit");
+
 btns.forEach((btn) =>{
     btn.addEventListener("click", (e)=>{
         storeDigit(e.target.textContent);
         updateDisplay(b);
+    });
+});
+
+chosenOperator.forEach((optr) =>{
+    optr.addEventListener("click", (e)=>{
+        
+        if(a == null){
+            a=b;
+            b= null;
+        }
+        else{
+            let result= operate(operator, a, b);
+            b=null;
+            a=String(result);
+            updateDisplay(a);
+        }
+        operator= e.target.textContent;
     });
 });
 
