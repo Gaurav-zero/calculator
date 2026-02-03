2e39b69b95df200c643fea221ad0b94813e60c7d
Gaurav <183895839+Gaurav-zero@users.noreply.github.com>
2026-02-03 11:48:01 +0530

    create functions for different operations
    created functions for all operators(+,-,*,/) and variables for
    operand and operator. Also created an operate function, which performs
    one of the operations based on the operator given

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("checkingScriptConnection");
+function add(a,b){return a+b};
+function subtract(a,b){return a-b};
+function multiply(a,b){return a*b};
+function divide(a,b){return a/b};
+function operate(operator, a, b){
+    switch (operator){
+        case "+":
+            return add(a,b);
+            break;
+        case "-":
+            return subtract(a,b);
+            break;
+        case "*":
+            return multiply(a,b);
+            break;
+        case "/":
+            return divide(a,b);
+            break;
+    }
+}
+
+let firstOperand;
+let secondOperand;
+let operator;
+
+console.log("check");
